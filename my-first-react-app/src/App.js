a67793e44f13275993e228928be6b505fc3e5040
Matt <mhur0724@gmail.com>
2023-06-03 16:54:09 -0400

    updating code

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     {title: "mario's bday bash", id: 1},
     {title: "bowser's live stream", id: 2},
@@ -15,10 +15,15 @@ function App() {
     }))
     console.log(id);
   }
-
   return (
     <div className="App">
-        {events.map((event, index)=> (
+     {showEvents && <div>
+        <button onClick={() => setShowEvents(false)}>hide events</button>
+      </div>}
+      {!showEvents && <div>
+        <button onClick={() => setShowEvents(true)}>show events</button>
+      </div>}
+        {showEvents && events.map((event, index)=> (
           <div key={event.id}> 
             <h2>{index} - {event.title}</h2>
             <button onClick={() => handleClick(event.id)}>delete event</button>
