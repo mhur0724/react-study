176c1ca55e433855253333b1a2ddc55ea120ba17
Matthew Hur <matthew.hur@kohls.com>
2023-06-03 15:40:24 -0500

    created app

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [events, setEvents] = useState([
+    {title: "mario's bday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3},
+  ])
+
+  const handleClick = (id) => {
+    setEvents(events.filter((event) => {
+      return id !== event.id
+    }))
+    console.log(id);
+  }
+
+  return (
+    <div className="App">
+        {events.map((event, index)=> (
+          <div key={event.id}> 
+            <h2>{index} - {event.title}</h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default App;
