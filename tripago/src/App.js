9d4ef5116dd220c77d3aa4dcc4f129db10299f74
Matt <mhur0724@gmail.com>
2023-08-06 10:58:50 -0400

    starting card game

--- tripago/src/App.js
+++ tripago/src/App.js
@@ -1,9 +1,14 @@
+import {useState} from 'react'
+import TripList from './components/TripList';
 import './App.css';
 
 function App() {
+  const [showTrips, setShowTrips] = useState(true)
+  
   return (
     <div className="App">
-
+      <button onClick={() => setShowTrips(false)}>hide trips</button>
+      {showTrips && <TripList/>}
     </div>
   );
 }
